a4c6b9048c9ad997602bbd54123c1616ee1d82e2
azharmj <46325751+azharmj@users.noreply.github.com>
2022-12-13 11:49:29 +0530

    Update checker.c

--- checker.c
+++ checker.c
@@ -1,23 +1,32 @@
-#include <stdio.h>
-#include <assert.h>
-#define CheckRange(X,Y,Z) (X<=Y || X>=Z)?0:1
-int Check_ChargeRate(float chargeRate); 
-int batteryIsOk(float temperature, float soc, float chargeRate)
-{int batterystate = 1;
-batterystate = CheckRange(temperature,0 , 45);
-batterystate &= CheckRange(soc,20, 80);
-batterystate &= Check_ChargeRate(chargeRate);
-return batterystate;}
-int Check_ChargeRate(float chargeRate)
+#include <stdio.h>
+#include <assert.h>
+
+#define CheckRange(X,Y,Z) (X<=Y || X>=Z)?0:1
+
+int Check_ChargeRate(float chargeRate);
+
+int batteryIsOk(float temperature, float soc, float chargeRate)
 {
-	if(chargeRate>0.8)
-		return 0;
-	else
-		return 1;
-	}  
-int main()
+  int batterystate = 1;
+  batterystate =  CheckRange(temperature,0 , 45);
+  batterystate &= CheckRange(soc,20, 80);
+  batterystate &= Check_ChargeRate(chargeRate);
+  return batterystate;
+}
+
+int Check_ChargeRate(float chargeRate)
 {
-	 assert(batteryIsOk(25, 70, 0.7));
-	 assert(!batteryIsOk(50, 85, 0));
-	 
-} 
+   if(chargeRate>0.8)
+   return 0;
+   else
+   return 1;
+}
+
+
+int main()
+{
+ 
+  assert(batteryIsOk(25, 70, 0.7));
+  assert(!batteryIsOk(50, 85, 0));
+
+}
